b6e2349441ac4ce8daf08807509aa0af1b4897da
Corey <corey.amano@gmail.com>
2021-06-08 11:50:42 -0700

    add response to question 3

--- conditionals_practice.rb
+++ conditionals_practice.rb
@@ -23,7 +23,14 @@ end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
+y = 3
+z = 5
 
+if y < 10 && z < 10
+  p 1
+else
+  p 0
+end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
