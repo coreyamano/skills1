9f6be8b45fc289def5c0c1cc88544efa174be88c
Corey <corey.amano@gmail.com>
2021-06-08 12:33:28 -0700

    add response to question 9

--- conditionals_practice.rb
+++ conditionals_practice.rb
@@ -91,4 +91,13 @@ end
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
+fire = 28
+ice = 4
+
+if fire < 0 && ice > 0
+  p 1
+else
+  p 0
+end
+
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
