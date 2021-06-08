5772d218134855617aa2bf81a6cd9ce053dbf209
Corey <corey.amano@gmail.com>
2021-06-08 12:23:21 -0700

    add response to question 7

--- conditionals_practice.rb
+++ conditionals_practice.rb
@@ -69,6 +69,14 @@ end
 
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
+year = 777
+
+if year < 0
+  p 1776
+else
+  p 1979
+end
+
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
