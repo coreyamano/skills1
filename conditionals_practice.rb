9654f82a7df1e9a63420eb6e028e0ec8cf27d55a
Corey <corey.amano@gmail.com>
2021-06-08 12:37:55 -0700

    add response to question 10

--- conditionals_practice.rb
+++ conditionals_practice.rb
@@ -101,3 +101,17 @@ else
 end
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+final = 44
+
+if final > 80
+  p 5
+elsif final > 60
+  p 4
+elsif final > 40
+  p 3
+elsif final > 20
+  p 2
+else
+  p 1
+end
